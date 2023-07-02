545c277a31fe719d8b4cb7ce237e86f771e1d18a
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-02 23:48:41 +0000

    32 - QuerySet Introduction

--- members/views.py
+++ members/views.py
@@ -27,7 +27,7 @@ def main(request):
 
 
 def testing(request):
-    mymembers = Member.objects.all().values()
+    mymembers = Member.objects.all()
     template = loader.get_template('testing.html')
     context = {
         'fruits': ['Apple', 'Banana', 'Cherry'],
